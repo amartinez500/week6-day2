4bd17823a0042606d13fcdc015bd37be51a0e67d
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2023-02-16 18:40:45 +0000

    learned how to use with open

---
+++ main.py
@@ -0,0 +1,19 @@
+# remember to fork this and push to github
+#f = open('employees.txt', 'a')
+#f.write(" Dwight is a loving man!!!")
+#read give us everything
+#readline gives us the firstline
+#print(f.readline())
+
+#f.close()
+#with open("test.txt" , 'r') as f:
+
+  #for line in f:
+    #print(line, end='')
+  
+  #f_contents= f.read()
+  #f.write("kdkdkdskdkdksdksdks")
+  #print(f)
+
+with open('period7,txt','w') as f:
+  f.write('this mess is over')
